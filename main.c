980c0a1f1da028917fdb08b21edf62bd0136d386
Fabian Noriega <fabian@rabbitai.de>
2024-11-10 19:58:36 +0100

    add main time loop to compute pressure field

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
-#define WIDTH 512
-#define HEIGHT 512
+#define WIDTH 1024
+#define HEIGHT 1024
 
 //finite difference method to compute horizontal derivative
 void d2P_dX2(float *P, float *dPdX, float dx){
@@ -22,7 +23,18 @@ void d2P_dZ2(float *P, float *dPdZ, float dz){
 //finite difference method to compute second time derivative. therefore, pressure field at time t+1
 void d2P_dt2(float *P, float *P0, float *P1, float *dPdX, float *dPdZ, float *velocity, float dx, float dz, float dt){
     for(int i=0; i<WIDTH*HEIGHT; i++){
-        P1[i] = 2*P[i] - P0[i] + (velocity[i]*velocity[i]*dt*dt)*(dPdX[i] + dPdZ[i]);
+        P[i] = 2*P0[i] - P1[i] + (velocity[i]*velocity[i]*dt*dt)*(dPdX[i] + dPdZ[i]);
+    }
+}
+
+
+//use ricker wavelet to create source
+void create_source(float *source, int timesamples, float dt, float freq){
+
+    float wp = 2.0*M_PI*freq;
+    for(int i=0; i<timesamples; i++){
+        float tau = i*dt;
+        source[i] = (1 - 0.5*wp*wp*tau*tau)*exp(-0.25*wp*wp*tau*tau);
     }
 }
 
@@ -34,8 +46,9 @@ int main(){
     float dx = 25.0;
     float dz = 25.0;
     float dt = 0.001;
+    float freq = 3.0;
 
-    float *source = calloc(WIDTH*HEIGHT, sizeof(float));
+    float *source = calloc(timesamples, sizeof(float));
     float *velocity = calloc(WIDTH*HEIGHT, sizeof(float));
     float *P = calloc(WIDTH*HEIGHT, sizeof(float));
     float *P0 = calloc(WIDTH*HEIGHT, sizeof(float));
@@ -47,7 +60,22 @@ int main(){
         velocity[i] = 2500.0;
     }
 
+    //initialize source
+    create_source(source, timesamples, dt, freq);
 
+    for(int t=0; t<timesamples; t++){
+        //update pressure field
+        d2P_dX2(P, dPdX, dx);
+        d2P_dZ2(P, dPdZ, dz);
+        d2P_dt2(P, P0, P1, dPdX, dPdZ, velocity, dx, dz, dt);
+
+        //apply source
+        P[WIDTH*HEIGHT/2] += source[t];
+
+        //update pressure field
+        P1 = P0;
+        P0 = P;
+    }
 
     return 0;
 }
